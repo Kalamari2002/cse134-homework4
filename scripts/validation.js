432287762d9b8f076d8b4da30561fdd08fe8171e
Gabriel Ikezaki <gabriel.ikezaki@gmail.com>
2025-02-26 11:48:15 -0800

    made adjustments to css error feedback, info now display dummy character count text

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,18 +1,25 @@
 const nameInput = document.querySelector('#contact-name');
 const nameErr = document.querySelector(`output[name='name-error-output']`);
-const nameInfo = document.querySelector(`output[name='name-info-output']`)
+const nameInfo = document.querySelector(`output[name='name-info-output']`);
 
-console.log(nameErr);
+const flashKeyFrames = [{backgroundColor: "#ec9595"}, {backgroundColor: "#eeeccf"}];
+const flashTiming = { duration: 2000 };
+
+const fadeoutFrames = [{opacity: 1}, {opacity: 1}, {opacity: 1}, {opacity: 1}, {opacity: 0}];
+const fadeoutTiming = { duration: 4000 };
+
+console.log(nameInput);
 nameInput.addEventListener("input", (event)=>{
-    nameInput.setCustomValidity('');
+    nameInput.setCustomValidity("");
 
     if(!nameInput.checkValidity()){
         console.log("NAME ERROR");
-        nameErr.hidden = false;
-        nameInfo.hidden = false;
+        // nameInput.animate(flashKeyFrames,flashTiming);
+        // nameErr.animate(fadeoutFrames, fadeoutTiming);
+        // nameInfo.animate(fadeoutFrames, fadeoutTiming);
     }else{
-        nameErr.hidden = true;
-        nameInfo.hidden = true;
+        //nameInfo.hidden = true;
+
     }
 });
 
